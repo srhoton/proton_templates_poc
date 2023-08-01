690c57fae1784415b0dbcc61fcc7040ac41a7b8f
Stephen Rhoton <stephen.rhoton@dealerware.com>
2023-08-01 10:38:07 -0500

    Update v4 configs

---
+++ vpc-creation/v4/infrastructure/network.tf
@@ -0,0 +1,6 @@
+module "default_network" {
+  source = "git@github.com:srhoton/tf-module-network.git"
+  env_name = var.environment_name
+  base_cidr_block = var.environment.inputs.base_cidr_block
+  enable_nat_gateway = false
+}
